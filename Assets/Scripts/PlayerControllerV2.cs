e93042cc4b3ee7ef324e282394ec43c7ab0b107d
Dylan <110944869+DyPickard@users.noreply.github.com>
2025-03-01 17:02:57 -0800

    Reworked player movement to make smoother and more fluid

---
+++ Assets/Scripts/PlayerControllerV2.cs
@@ -0,0 +1,72 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerV2 : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 9f;
+    [SerializeField] private float acceleration = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private CinemachineCamera Camera;
+    [SerializeField] private float jumpHeight = 5f;
+    [SerializeField] private float airControlFactor = 0.2f;
+    private Vector2 moveInput;
+    private bool isGrounded = true;
+    private int jumpCount;
+    private Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        RotationUpdate();
+    }
+    private void FixedUpdate()
+    {
+        MovementUpdate();
+    }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log("Move Input: " + moveInput);
+    }
+    private void RotationUpdate()
+    {
+        Vector3 camForward = Camera.transform.forward;
+        camForward.y = 0f;
+        Quaternion targetRotation = Quaternion.LookRotation(camForward);
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.deltaTime));
+    }
+    private void MovementUpdate()
+    {
+        Vector3 moveDirection = (transform.right * moveInput.x) + (transform.forward * moveInput.y);
+        moveDirection.Normalize();
+        rb.AddForce(moveDirection * acceleration, ForceMode.Acceleration);
+        // limit acceleration to max speed
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
+    }
+    public void OnJump()
+    {
+        if (jumpCount < 2)
+        {
+            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, rb.linearVelocity.z);
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+            isGrounded = false;
+            jumpCount++;
+        }
+    }
+
+
+}
